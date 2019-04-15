1154edcfc83897e0050152b27943ad11ecc48529
ronaldo faustino <ohnirucse@msn.com>
2019-04-15 00:30:44 -0300

    cmd git

--- ruby/enjoeat_2/features/step_definitions/carrinho_steps.rb
+++ ruby/enjoeat_2/features/step_definitions/carrinho_steps.rb
@@ -30,3 +30,69 @@ Então("o valor deve ser de {string}") do |valor_total|
    #puts valor_total
    #jquery $('#cart tr:contains("Total:")');
 end
+
+#lista de produtos
+Dado("que os produtos desejados são:") do |table|
+  @product_list = table.hashes
+  puts @product_list
+end
+
+Quando("eu adiciono todos os itens") do
+  @product_list.each do |p|
+    p['quantidade'].to_i.times do
+      find('.menu-item-info-box', text: p["nome"].upcase).find('.add-to-cart').click
+    end
+  end
+end
+
+Então("vejo todos os itens no carrinho") do
+  cart = find("#cart")
+  #quantidade = 1
+  @product_list.each do |p|
+    expect(cart).to have_text "(#{p["quantidade"]}x) #{p["nome"]}"
+  end
+end
+
+# Remover itens do carrinho
+
+Dado("que eu tenho os seguintes itens no carrinho:") do |table|
+  @product_list = table.hashes
+  #puts @product_list
+  steps %{
+    Quando eu adiciono todos os itens
+  }
+
+  # @product_list.each do |p|
+  #   p['quantidade'].to_i.times do
+  #     find('.menu-item-info-box', text: p["nome"].upcase).find('.add-to-cart').click
+  #   end
+  # end
+end
+
+Quando("eu removo somente o item {int}") do |item|
+ #$('#cart table tbody tr')
+ cart = find("#cart")
+ cart.all("table tbody tr")[item].find(".danger").click
+end
+
+Quando("eu removo somente o {int}") do |item|
+ cart = find("#cart")
+ cart.all("table tbody tr")[item].find(".danger").click
+end
+
+Quando("eu removo todos os itens") do
+  @product_list.each_with_index do |value,idx|
+    #puts idx
+    cart = find("#cart")
+    cart.all("table tbody tr")[idx].find(".danger").click
+  end
+end
+
+Então("vejo a seguinte mensagem no carrinho {string}") do |mensagem|
+  cart = find("#cart")
+  expect(cart).to have_text mensagem
+end
+
+Quando("eu limpo meu carrinho") do
+  click_button "Limpar"
+end
